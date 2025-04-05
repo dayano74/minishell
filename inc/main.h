054e69a1e84460a1a74e6a3410c063be5c777eb6
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-05 13:24:32 +0900

    メモリ開放処理をdestructorにまとめる

--- inc/main.h
+++ inc/main.h
@@ -6,13 +6,14 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 12:50:38 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/03 13:20:59 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/05 13:04:21 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 
+# include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -6,11 +6,18 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 12:50:11 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/03 12:54:30 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/05 13:23:22 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+static void	_destructor(char *line)
+{
+	if (!line)
+		free(line);
+	rl_clear_history();
+}
 
 int	main(void)
 {
@@ -21,7 +28,9 @@ int	main(void)
 		line = readline("minish>");
 		if (!line)
 			break ;
+		add_history(line);
 		free(line);
 	}
+	_destructor(line);
 	return (EXIT_SUCCESS);
 }
