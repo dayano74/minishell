a6d88985f4607aec6d17531481a999a31c9105d6
cacapon <cacapon.project@gmail.com>
2025-04-06 16:29:02 +0900

    初期化処理追加

--- src/main.c
+++ src/main.c
@@ -6,11 +6,16 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 12:50:11 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/05 13:23:22 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/06 16:20:07 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+static void	_initialize(void)
+{
+	rl_clear_history();
+}
 
 static void	_destructor(char *line)
 {
@@ -23,6 +28,7 @@ int	main(void)
 {
 	char	*line;
 
+	_initialize();
 	while (1)
 	{
 		line = readline("minish>");
