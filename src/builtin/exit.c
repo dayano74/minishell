76c3ea23c64d841812622f4a6c69ab281354b878
Daiki Yano <dayano@c5r4s1.42tokyo.jp>
2025-04-13 22:22:27 +0900

    mvp built-in exit

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 21:20:48 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/13 21:40:27 by dayano           ###   ########.fr       */
+/*   Updated: 2025/04/13 22:15:31 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,34 @@ int	builtin_exit(int argc, char *argv[])
 		ft_putstr_fd(": too many arguments\n", STDERR_FILENO);
 		return (1);
 	}
+	if (printf("exit\n") < 0)
+		return (1);
 	exit(EXIT_SUCCESS);
+}
+
+#include <readline/readline.h>
+
+int	main(void)
+{
+	char	*line;
+	char	**strings_line;
+
+	while (1)
+	{
+		line = readline("minish>");
+		if (!line)
+			break ;
+		if (strcmp("exit", line) == 0)
+		{
+			strings_line = ft_split(line, ' ');
+			if (builtin_exit(3, strings_line) == 1)
+			{
+				printf("error\n");
+				free(line);
+				exit(EXIT_FAILURE);
+			}
+		}
+		free(line);
+	}
+	return (EXIT_SUCCESS);
 }
