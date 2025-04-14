f7f8f1609540009c39f865d59945e122ab48e103
cacapon <cacapon.project@gmail.com>
2025-04-14 12:35:01 +0900

    fix norm

--- tests/builtin/test_cd.c
+++ tests/builtin/test_cd.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:33:50 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/14 11:47:37 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/14 12:34:48 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 
 int	main(int argc, char **argv)
 {
-	char path[PATH_MAX];
+	char	path[PATH_MAX];
 
-    printf("cd %s\n", argv[1]);
+	printf("cd %s\n", argv[1]);
 	getcwd(path, PATH_MAX);
-    printf("before:path=%s\n", path);
-    builtin_cd(argc, argv);
+	printf("before:path=%s\n", path);
+	builtin_cd(argc, argv);
 	getcwd(path, PATH_MAX);
-    printf("after :path=%s\n", path);
-    return (0);
+	printf("after :path=%s\n", path);
+	return (0);
 }
