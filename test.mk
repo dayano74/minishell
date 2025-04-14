88ab5c21a8552bdec760716b2db78f7c18d279fa
cacapon <cacapon.project@gmail.com>
2025-04-14 21:02:02 +0900

    test.mkに統合

--- test.mk
+++ test.mk
@@ -1,17 +1,29 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Maketest.mk                                        :+:      :+:    :+:    #
+#    test.mk                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/04/14 11:48:18 by ttsubo            #+#    #+#              #
-#    Updated: 2025/04/14 11:49:08 by ttsubo           ###   ########.fr        #
+#    Updated: 2025/04/14 21:01:49 by ttsubo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+# usage
+# make -f test.mk
+# make -f test.mk clean
+
+I_FLG = -Iinc -Ilib/libft 
+L_FLG = -Llib/libft -lft -lreadline
+
+# 
+SRC = cd.c exit.c
+
 all:
-	cc tests/builtin/test_cd.c src/builtin/cd.c -Llib/libft -lft -Iinc -o test_cd.out
+	cc tests/builtin/test_cd.c src/builtin/cd.c -Llib/libft -lft -Iinc -Ilib/libft -o test_cd.out
+	cc tests/builtin/test_exit.c src/builtin/exit.c -Ilib/libft -Llib/libft -lft -Iinc -lreadline -o test_exit
 
 clean:
 	rm -f test_cd.out
+	rm -f test_exit
