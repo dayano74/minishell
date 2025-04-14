9f6220c29a055b90536b78bb1bc3cc48efe4d4d2
cacapon <cacapon.project@gmail.com>
2025-04-14 11:50:35 +0900

    cd テストコード作成

---
+++ tests/builtin/test_cd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_cd.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 11:33:50 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/04/14 11:47:37 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	char path[PATH_MAX];
+
+    printf("cd %s\n", argv[1]);
+	getcwd(path, PATH_MAX);
+    printf("before:path=%s\n", path);
+    builtin_cd(argc, argv);
+	getcwd(path, PATH_MAX);
+    printf("after :path=%s\n", path);
+    return (0);
+}
