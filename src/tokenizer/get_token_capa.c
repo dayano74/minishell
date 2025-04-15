92ad42f5676c0ef9e7d0974ff3b5474ad2255b86
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-15 14:12:11 +0900

    add get_token_capa

---
+++ src/tokenizer/get_token_capa.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_token_capa.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 13:59:15 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/04/15 14:02:28 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+/**
+ * @brief 文字列配列のキャパシティを計算します。
+ * 
+ * @param str 
+ * @return size_t
+ * @note 空白・リダイレクト・パイプの個数を求めています。
+ */
+size_t	get_token_capa(char *str)
+{
+	size_t	capa;
+	int		in_squote;
+	int		in_dquote;
+
+	capa = 1;
+	in_squote = 0;
+	in_dquote = 0;
+	while (*str)
+	{
+		if (*str == '\'' && !in_dquote)
+			in_squote = !in_squote;
+		else if (*str == '"' && !in_squote)
+			in_dquote = !in_dquote;
+		if (!in_squote && !in_dquote)
+		{
+			if (_ft_isspace(*str) || *str == '|')
+				capa++;
+			else if (*str == '<' || *str == '>')
+			{
+				capa++;
+				if (*(str + 1) == *str)
+					str++;
+			}
+		}
+		str++;
+	}
+	return (capa);
+}
