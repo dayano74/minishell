6eb67995c0e553f333a49f949916b0865e5d1aab
Daiki Yano <dayano@c3r1s2.42tokyo.jp>
2025-04-15 18:57:20 +0900

    make .h for struct for envp

---
+++ inc/struct.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 17:10:22 by dayano            #+#    #+#             */
+/*   Updated: 2025/04/15 17:11:19 by dayano           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+
+typedef struct s_env
+{
+	char			*value;
+	struct s_env	*next;
+}					t_env;
+
+typedef struct s_minish
+{
+	t_env			*env;
+}					t_minish;
+
+#endif
