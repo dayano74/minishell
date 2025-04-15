3448d85a88fa232cc545bff6a3a61c0404753dd1
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-15 16:11:48 +0900

    字句解析テスト用の関数を作成

---
+++ tests/tokenizer/test_tokenizer.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_tokenizer.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 15:56:39 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/04/15 16:10:02 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+int main(int argc, char **argv)
+{
+	char	**tokens;
+
+	(void)argc;
+	if (argc != 2)
+		return (printf("usage: ./test_tokenizer.out <any message>"), 1);
+	tokens = tokenizer(argv[1]);
+	while(*tokens)
+		printf("token=%s\n", *tokens++);
+	return (0);
+}
