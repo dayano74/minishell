5e4f1f4e00e3c6fbc2778bb599603d0a2da56bd7
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-17 11:45:25 +0900

    keywordを抽出する機能を追加

--- src/tokenizer/read_token.c
+++ src/tokenizer/read_token.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:33:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/17 11:37:18 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/17 11:43:49 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,18 @@ static char	*_read_quoted_token(t_tokenizer *tkn)
 	return (ft_substr(tkn->input, start, tkn->pos++ - start));
 }
 
+static char *_read_op_token(t_tokenizer *tkn)
+{
+	char c;
+
+	c = tkn->input[tkn->pos];
+	if ((c == '>' || c == '<') && tkn->input[tkn->pos + 1] == c)
+		return (tkn->pos +=2, ft_substr(tkn->input, tkn->pos - 2, 2));
+	if (c == '|' || c == '>' || c == '<')
+		return (tkn->pos +=1, ft_substr(tkn->input, tkn->pos - 1, 1));
+	return (NULL);
+}
+
 /**
  * @brief
  *
@@ -34,9 +46,13 @@ char	*read_token(t_tokenizer *tkn)
 {
 	int		start;
 	char	quote;
+	char	*op;
 
 	if (tkn->input[tkn->pos] == '\'' || tkn->input[tkn->pos] == '"')
 		return (_read_quoted_token(tkn));
+	op = _read_op_token(tkn);
+	if (op)
+		return (op);
 	if (tkn->input)
 		start = tkn->pos;
 	while (tkn->input[tkn->pos] && !ft_isspace(tkn->input[tkn->pos]))
