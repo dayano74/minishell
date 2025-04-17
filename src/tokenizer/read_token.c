cc4a178079acff7a081d5147f9ec8e9eac6004a5
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-17 11:39:16 +0900

    refeactoring Static関数に切り出し

--- src/tokenizer/read_token.c
+++ src/tokenizer/read_token.c
@@ -6,17 +6,29 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:33:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/15 16:51:03 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/17 11:37:18 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokenizer.h"
 
+static char	*_read_quoted_token(t_tokenizer *tkn)
+{
+	char	quote;
+	int		start;
+
+	quote = tkn->input[tkn->pos++];
+	start = tkn->pos;
+	while (tkn->input[tkn->pos] && tkn->input[tkn->pos] != quote)
+		tkn->pos++;
+	return (ft_substr(tkn->input, start, tkn->pos++ - start));
+}
+
 /**
- * @brief 
- * 
- * @param tkn 
- * @return char* 
+ * @brief
+ *
+ * @param tkn
+ * @return char*
  */
 char	*read_token(t_tokenizer *tkn)
 {
@@ -24,14 +36,9 @@ char	*read_token(t_tokenizer *tkn)
 	char	quote;
 
 	if (tkn->input[tkn->pos] == '\'' || tkn->input[tkn->pos] == '"')
-	{
-		quote = tkn->input[tkn->pos++];
+		return (_read_quoted_token(tkn));
+	if (tkn->input)
 		start = tkn->pos;
-		while (tkn->input[tkn->pos] && tkn->input[tkn->pos] != quote)
-			tkn->pos++;
-		return (ft_substr(tkn->input, start, tkn->pos++ - start));
-	}
-	start = tkn->pos;
 	while (tkn->input[tkn->pos] && !ft_isspace(tkn->input[tkn->pos]))
 		tkn->pos++;
 	return (ft_substr(tkn->input, start, tkn->pos - start));
