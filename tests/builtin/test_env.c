dde66ea83a9b986b8884bb9916ad066739179a2a
dayano74 <kiixxn00@gmail.com>
2025-04-18 16:13:57 +0900

    Fix test case of built-in env

--- tests/builtin/test_env.c
+++ tests/builtin/test_env.c
@@ -6,14 +6,57 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 10:24:45 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/15 10:25:03 by dayano           ###   ########.fr       */
+/*   Updated: 2025/04/18 16:11:30 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+static size_t	_count_words(char **cmds)
+{
+	size_t	i;
+
+	i = 0;
+	while (cmds[i])
+		i++;
+	return (i);
+}
+
+static void	cmds_free(char **cmds)
+{
+	size_t	i;
+	size_t	length;
+
+	i = 0;
+	length = _count_words(cmds);
+	while (i < length)
+		free(cmds[i++]);
+	free(cmds);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	builtin_env(argc, argv, envp);
-	return (0);
+	char		*line;
+	char		**cmds;
+	t_minish	*minish;
+
+	(void)argc;
+	(void)argv;
+	minish = initialize(envp);
+	while (1)
+	{
+		line = readline("minish>");
+		if (!line)
+			break ;
+		if (line[0] == '\0')
+			continue ;
+		cmds = ft_split(line, ' ');
+		if (!ft_strcmp(cmds[0], "env"))
+			builtin_env(_count_words(cmds), cmds, minish);
+		cmds_free(cmds);
+		free(line);
+	}
+	cleanup_minish(minish);
+	free(line);
+	return (EXIT_SUCCESS);
 }
