76f381daa17cc1aa8bbf3689a5bc3ba2ce15f874
cacapon <cacapon.project@gmail.com>
2025-04-20 20:01:20 +0900

    fix norm

--- src/builtin/builtin_utils.c
+++ src/builtin/builtin_utils.c
@@ -6,21 +6,21 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 19:46:40 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/20 19:53:02 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/20 20:01:04 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "main.h"
+#include "main.h"
 
 /**
  * @brief "<name>: <mes>\n"形式でSTDERRに出力します。
- * 
- * @param name 
- * @param mes 
+ *
+ * @param name
+ * @param mes
  */
 void	error_mes(char *name, char *mes)
 {
 	ft_putstr_fd(name, STDERR_FILENO);
-    ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putendl_fd(mes, STDERR_FILENO);
 }
