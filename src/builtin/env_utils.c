292ae1c5ee9be23b2bb09ebc29e737dfd70fbb57
cacapon <cacapon.project@gmail.com>
2025-04-20 19:02:43 +0900

    keyの情報で事足りるので未使用になったget_env_nameは削除

--- src/builtin/env_utils.c
+++ src/builtin/env_utils.c
@@ -6,25 +6,11 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 10:26:59 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/20 18:55:27 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/20 19:02:10 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
-
-char	*get_env_name(const char *env_entry)
-{
-	char	*eq_pos;
-	char	*env_name;
-	int		name_len;
-
-	eq_pos = ft_strchr(env_entry, '=');
-	if (!eq_pos)
-		return (NULL);
-	name_len = eq_pos - env_entry;
-	env_name = ft_substr(env_entry, 0, name_len);
-	return (env_name);
-}
 
 void	free_env_content(char *value)
 {
