b2eb794e26128e88af6dcbcda88f14b22a974fb0
dayano74 <kiixxn00@gmail.com>
2025-04-20 21:19:27 +0900

    fix main func for parse_line and invoke_cmd

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 12:50:11 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/20 20:41:20 by dayano           ###   ########.fr       */
+/*   Updated: 2025/04/20 21:16:16 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,33 @@
 
 static void	destroy_minish(t_minish *minish)
 {
-	if (!minish)
-		return ;
-	free(minish);
+	cleanup_minish(minish);
 	rl_clear_history();
 }
 
-static bool	prompt(t_minish *minish)
+/**
+ * @brief bool型が利用できるみたいなので、使用してみました。
+ * @brief parse_command_lineのプロトタイプを勝手に作成しているので、好きなように改変していただいて大丈夫です
+ * @param program_nam
+ * @param minish
+ * @return true
+ * @return false
+ */
+static bool	prompt(char *program_name, t_minish *minish)
 {
 	char	*line;
+	t_cmd	*cmd;
 
 	line = readline("minish>");
 	if (!line)
 		return (false);
 	if (line[0] != '\0')
 		add_history(line);
+	cmd = parse_command_line(line);
+	if (!cmd)
+		return (error_mes(program_name, ": syntax error\n"), false);
+	if (cmd->argc > 0)
+		invoke_commands(cmd);
 	free(line);
 	return (true);
 }
@@ -36,12 +48,13 @@ static bool	prompt(t_minish *minish)
 int	main(int argc, char **argv, char **envp)
 {
 	t_minish	*minish;
+	char		*program_name;
 
 	(void)argc;
-	(void)argv;
+	program_name = argv[0];
 	minish = initialize(envp);
 	minish_signal();
-	while (prompt(minish))
+	while (prompt(program_name, minish))
 		;
 	destroy_minish(minish);
 	return (0);
