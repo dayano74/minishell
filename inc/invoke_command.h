91b99e145fe2fdca86dfbbc5e7594e7bf6a3a915
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-21 13:52:09 +0900

    fix norm

--- inc/invoke_command.h
+++ inc/invoke_command.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   invoke_command.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 21:21:13 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/21 13:31:54 by dayano           ###   ########.fr       */
+/*   Updated: 2025/04/21 13:51:41 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INVOKE_COMMAND
-# define INVOKE_COMMAND
+#ifndef INVOKE_COMMAND_H
+# define INVOKE_COMMAND_H
 
 # include "cmd.h"
 
