3b8a42cf85507963f886583bb4d8fa4f04edd10b
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-24 14:27:27 +0900

    call setup_cmds

--- src/perser/perser.c
+++ src/perser/perser.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:14:32 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/24 13:38:07 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/24 14:27:15 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,9 @@ t_cmd	*perser(char **tokens)
 	t_cmd	*cmd;
 
 	cmds = allocate_cmds(tokens);
+	if (!cmds)
+		return (NULL);
+	cmds = setup_cmds(cmds, tokens);
 	if (!cmds)
 		return (NULL);
 	return (cmds);
