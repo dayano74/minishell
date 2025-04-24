817fad5ab1342aab08e52db05fbe2471f3a24d88
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-24 14:59:07 +0900

    add allocate_argv

--- src/perser/setup_cmds.c
+++ src/perser/setup_cmds.c
@@ -6,11 +6,29 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:38:36 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/24 14:24:24 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/24 14:56:34 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "perser.h"
+
+static int _allocate_argv(t_cmd *cmd, char **tokens)
+{
+	size_t token_i;
+	size_t argc;
+
+	token_i = 0;
+	argc = 0;
+	while (tokens[token_i] && !is_separator(tokens[token_i]))
+	{
+		token_i++;
+		argc++;
+	}
+	cmd->argv = ft_calloc(argc + 1, sizeof(char *));
+	if (!cmd->argv)
+		return (1);
+	return (0);
+}
 
 static void _set_type(t_cmd *cmd, char *token)
 {
@@ -37,6 +55,7 @@ t_cmd **setup_cmds(t_cmd **cmds, char **tokens)
 	cmd_i = 0;
 	arg_i = 0;
 	_set_type(cmds[0], tokens[token_i]);
+	_allocate_argv(cmds[0], tokens);
 	while (tokens[token_i])
 	{
 		if (is_separator(tokens[token_i]))
@@ -45,6 +64,8 @@ t_cmd **setup_cmds(t_cmd **cmds, char **tokens)
 			cmds[cmd_i]->argc = arg_i;
 			arg_i = 0;
 			cmd_i++;
+			_set_type(cmds[cmd_i], tokens[token_i]);
+			_allocate_argv(cmds[cmd_i], &tokens[token_i]);
 		}
 		else
 		{
