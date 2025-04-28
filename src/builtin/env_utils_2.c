1aff24175c2e7328f458f8f45dcdbb0b216c8626
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-28 16:32:28 +0900

    fix split_key_value

--- src/builtin/env_utils_2.c
+++ src/builtin/env_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:17:22 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/28 12:44:29 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/28 16:32:00 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,50 @@ void	handle_error_and_exit(const char *func_name, t_minish *minish)
 	exit(EXIT_FAILURE);
 }
 
+static void	_set_len_and_pos(char *str, size_t *key_len, size_t *val_start)
+{
+	const char	*sep;
+
+	sep = ft_strnstr(str, "+=", ft_strlen(str));
+	if (sep)
+	{
+		*key_len = sep - str;
+		*val_start = *key_len + 2;
+	}
+	else
+	{
+		sep = ft_strchr(str, '=');
+		if (sep)
+		{
+			*key_len = sep - str;
+			*val_start = *key_len + 1;
+		}
+		else
+		{
+			*key_len = ft_strlen(str);
+			*val_start = 0;
+		}
+	}
+}
+
 int	split_key_value(char *str, char **key_out, char **val_out)
 {
-	size_t	env_len;
-	size_t	eq_pos;
+	size_t	key_len;
+	size_t	val_start;
 
-	env_len = ft_strlen(str);
-	eq_pos = ft_strlen_until(str, '=');
-	if (eq_pos == env_len)
+	if (!str || !key_out || !val_out)
 		return (1);
-	*key_out = ft_substr(str, 0, eq_pos);
-	*val_out = ft_substr(str, eq_pos + 1, env_len - eq_pos - 1);
-	if (!key_out || !val_out)
+	*key_out = NULL;
+	*val_out = NULL;
+	_set_len_and_pos(str, &key_len, &val_start);
+	*key_out = ft_substr(str, 0, key_len);
+	if (!*key_out)
 		return (1);
+	if (val_start > 0)
+	{
+		*val_out = ft_substr(str, val_start, ft_strlen(str) - val_start);
+		if (!*val_out)
+			return (free(*key_out), *key_out = NULL, 1);
+	}
 	return (0);
 }
