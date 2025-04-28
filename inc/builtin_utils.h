f679fa4f3f8a739cd2193283876d1d6632f9c5de
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-28 16:37:03 +0900

    fix build error

--- inc/builtin_utils.h
+++ inc/builtin_utils.h
@@ -6,13 +6,14 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 19:47:50 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/28 16:09:32 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/28 16:36:46 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_UTILS_H
 # define BUILTIN_UTILS_H
 
+# include <stddef.h>
 # include <stdbool.h>
 
 typedef struct s_export_exec_bool
