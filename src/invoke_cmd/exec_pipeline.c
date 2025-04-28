9a6b46a3ae188f3669e63e3c52be683f34c43df9
dayano74 <kiixxn00@gmail.com>
2025-04-28 21:57:31 +0900

    fix execpipe_line

--- src/invoke_cmd/exec_pipeline.c
+++ src/invoke_cmd/exec_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:14:15 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/28 14:12:13 by dayano           ###   ########.fr       */
+/*   Updated: 2025/04/28 21:40:44 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ void	exec_pipeline(t_cmd *cmd_head, t_minish *minish)
 	int		fds1[2];
 	int		fds2[2];
 
-	(void)minish;
 	cmd = cmd_head;
 	initialize_fds(fds1, fds2);
 	while (cmd && !is_redirect(cmd))
@@ -62,11 +61,11 @@ void	exec_pipeline(t_cmd *cmd_head, t_minish *minish)
 				close(fds1[0]);
 			if (fds1[1] != -1)
 				close(fds1[1]);
+			cmd = cmd->next;
 			continue ;
 		}
 		if (!is_head(cmd, cmd_head))
 		{
-			close(STDIN_FILENO);
 			dup2(fds1[0], STDIN_FILENO);
 			close(fds1[0]);
 			close(fds1[1]);
@@ -74,20 +73,21 @@ void	exec_pipeline(t_cmd *cmd_head, t_minish *minish)
 		if (!is_tail(cmd))
 		{
 			close(fds2[0]);
-			close(STDOUT_FILENO);
 			dup2(fds2[1], STDOUT_FILENO);
 			close(fds2[1]);
 		}
 		if ((cmd->next != NULL) && is_redirect(cmd->next))
 		{
-			redirect_stdout(cmd->next);
+			redirect(cmd->next);
 		}
-		if (!is_builtin(cmd))
+		if (is_builtin(cmd))
 		{
-			execute_cmd(cmd, minish);
-			exit(EXIT_FAILURE);
+			exec_unit_builtin(cmd);
+			exit(EXIT_SUCCESS);
 		}
-		cmd = cmd->next;
+		execute_cmd(cmd, minish);
+		cmd->status = EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 }
 
