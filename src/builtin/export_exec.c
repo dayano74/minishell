6db47f4bf273d98b8df2640a5c6f474e60435b35
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-28 15:43:37 +0900

    add _add_key_value

--- src/builtin/export_exec.c
+++ src/builtin/export_exec.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:11:06 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/28 15:41:33 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/28 15:43:07 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,18 @@ static int	_append_value(char *key, char *value, t_minish *minish)
 
 static int	_add_key_value(char *key, char *value, t_minish *minish)
 {
-	// t_env	*node;
-	// node = ft_calloc(1, sizeof(t_env));
-	// if (!node)
-	// 	return (EXIT_FAILURE);
-	// split_key_value(arg, node->key, node->value);
-	// if (!node->key || !node->value)
-	// 	return (free(node->key), free(node->value), free(node), EXIT_FAILURE);
+	t_env	*node;
+	node = ft_calloc(1, sizeof(t_env));
+	if (!node)
+		return (EXIT_FAILURE);
+	node->key = ft_strdup(key);
+	if (!node->key)
+		return (free(node), EXIT_FAILURE);
+	node->value = ft_strdup(value);
+	if (!node->value)
+		return (free(node->key), free(node), EXIT_FAILURE);
+	node->is_exported = 1;
+	add_env_back(minish->env, node);
 	return (EXIT_SUCCESS);
 }
 
