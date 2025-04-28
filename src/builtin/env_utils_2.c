fea22a061be03e405751569dae0efd0825c84643
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-28 12:35:06 +0900

    fix norm

--- src/builtin/env_utils_2.c
+++ src/builtin/env_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:17:22 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/21 12:21:26 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/28 12:29:12 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,6 @@ int	split_key_value(char *str, char **key_out, char **val_out)
 
 	env_len = ft_strlen(str);
 	eq_pos = ft_strlen_until(str, '=');
-
 	if (eq_pos == env_len)
 		return (0);
 	*key_out = ft_substr(str, 0, eq_pos);
