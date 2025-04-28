813cd2e9942bc505cad1bed9700d72f85c3dace6
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-28 15:39:29 +0900

    fix norm

--- inc/builtin_utils.h
+++ inc/builtin_utils.h
@@ -6,14 +6,23 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 19:47:50 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/28 12:39:00 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/28 15:30:13 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_UTILS_H
 # define BUILTIN_UTILS_H
 
-void	error_mes(char *name, char *mes);
-int		print_sorted_env(t_env *head);
+# include <stdbool.h>
+
+typedef struct s_export_exec_bool
+{
+	bool	exists;
+	bool	has_plus;
+	bool	has_eq;
+}			t_export_exec_bool;
+
+void		error_mes(char *name, char *mes);
+int			print_sorted_env(t_env *head);
 
 #endif
--- src/builtin/export_exec.c
+++ src/builtin/export_exec.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:11:06 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/28 15:12:33 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/28 15:31:58 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ static int	_append_value(char *key, char *value, t_minish *minish)
 static int	_add_key_value(char *key, char *value, t_minish *minish)
 {
 	// t_env	*node;
-
 	// node = ft_calloc(1, sizeof(t_env));
 	// if (!node)
 	// 	return (EXIT_FAILURE);
@@ -38,7 +37,6 @@ static int	_add_key_value(char *key, char *value, t_minish *minish)
 static int	_add_key(char *key, t_minish *minish)
 {
 	// t_env	*node;
-
 	// node = ft_calloc(1, sizeof(t_env));
 	// if (!node)
 	// 	return (EXIT_FAILURE);
@@ -47,30 +45,26 @@ static int	_add_key(char *key, t_minish *minish)
 
 int	export_exec(int argc, char **argv, t_minish *minish)
 {
-	size_t	i;
-	char	*key;
-	char	*value;
+	size_t				i;
+	char				*key;
+	char				*value;
+	t_export_exec_bool	bools;
 
 	i = 1;
 	while (i < argc)
 	{
 		split_key_value(argv[i], key, value);
-		if (get_env_value(minish->env, key))
-		{
-			if (ft_strnstr(argv[i], "+=", ft_strlen(argv[i])))
-				_append_value(key, value, minish);
-			else if (ft_strchr(argv[i], '='))
-				_update_value(key, value, minish);
-		}
+		bools.exists = get_env_value(minish->env, key) != NULL;
+		bools.has_plus = ft_strnstr(argv[i], "+=", ft_strlen(argv[i])) != NULL;
+		bools.has_eq = ft_strchr(argv[i], '=') != NULL;
+		if (bools.exists && bools.has_plus)
+			_append_value(key, value, minish);
+		else if (bools.exists && !bools.has_plus && bools.has_eq)
+			_update_value(key, value, minish);
+		else if (!bools.exists && (bools.has_plus || bools.has_eq))
+			_add_key_value(key, value, minish);
 		else
-		{
-			if (ft_strnstr(argv[i], "+=", ft_strlen(argv[i])))
-				_append_value(key, value, minish);
-			else if (ft_strchr(argv[i], '='))
-				_update_value(key, value, minish);
-			else
-				_add_key(key, minish);
-		}
+			_add_key(key, value);
 		free(key);
 		free(value);
 		i++;
