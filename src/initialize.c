f6af23622c44b51cdcbd404e01a69c3341d5e9e4
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-29 16:17:44 +0900

    nodeの処理を充実化

--- src/initialize.c
+++ src/initialize.c
@@ -6,13 +6,13 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:42:46 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/20 18:39:53 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/29 16:17:19 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-static void	_split_key_value(char *env_val, char **key_out, char **val_out)
+static int	_split_key_value(char *env_val, char **key_out, char **val_out)
 {
 	size_t	env_len;
 	size_t	eq_pos;
@@ -22,9 +22,31 @@ static void	_split_key_value(char *env_val, char **key_out, char **val_out)
 	*key_out = NULL;
 	*val_out = NULL;
 	if (eq_pos == ft_strlen(env_val))
-		return ;
+		return (1);
 	*key_out = ft_substr(env_val, 0, eq_pos);
 	*val_out = ft_substr(env_val, eq_pos + 1, env_len - eq_pos - 1);
+	if (!*key_out || !*val_out)
+		return (1);
+	return (0);
+}
+
+static void	_free_node(t_env *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+static void	_free_nodes(t_env **head)
+{
+	t_env	*tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		_free_node(*head);
+		*head = tmp;
+	}
 }
 
 static t_env	*create_envp_list(char **envp)
@@ -42,9 +64,8 @@ static t_env	*create_envp_list(char **envp)
 		node = ft_calloc(1, sizeof(t_env));
 		if (!node)
 			return (NULL);
-		_split_key_value(envp[i], &(node->key), &(node->value));
-		if (!node->key || !node->value)
-			return (free(node), NULL);
+		if (!_split_key_value(envp[i], &(node->key), &(node->value)))
+			return (_free_nodes(&head), _free_node(node), NULL);
 		node->next = NULL;
 		if (!head)
 			head = node;
