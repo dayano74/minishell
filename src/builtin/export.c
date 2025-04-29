3fe37532b3729dcdcf8704627320c7b3e35d120e
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-29 13:58:00 +0900

    複数入力対応

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:35:46 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/28 16:43:13 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/29 13:56:56 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	builtin_export(int argc, char *argv[], t_minish *minish)
 {
 	if (argc == 1)
 		return (print_sorted_env(minish->env));
-	if (argc == 2)
+	if (argc >= 2)
 		return (export_exec(argc, argv, minish));
 	return (EXIT_SUCCESS);
 }
