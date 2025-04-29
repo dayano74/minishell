e5cd850c30df98c4e2473b544a980b0dd55e3837
dayano74 <kiixxn00@gmail.com>
2025-04-29 11:49:02 +0900

    add lib to use waitpid in wait_pipeline func

--- inc/exec_pipeline.h
+++ inc/exec_pipeline.h
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:12:27 by dayano            #+#    #+#             */
-/*   Updated: 2025/04/24 14:17:43 by dayano           ###   ########.fr       */
+/*   Updated: 2025/04/29 11:48:05 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define EXEC_PIPELINE_H
 
 # include "cmd.h"
+# include <sys/types.h>
+# include <sys/wait.h>
 
 void	exec_pipeline(t_cmd *cmd_head, t_minish *minish);
 int		wait_pipeline(t_cmd *cmd_head);
