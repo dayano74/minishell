9b13a4a9e1d441bf9bbc39c353cf65f6ed3c8914
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-04-29 11:51:44 +0900

    有効値チェック追加

--- src/builtin/export_exec.c
+++ src/builtin/export_exec.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:11:06 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/04/29 11:33:39 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/04/29 11:51:26 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ static int	_update_value(char *key, char *value, t_minish *minish)
 {
 	t_env	*node;
 
+	if (!is_valid_key(key))
+		return (EXIT_FAILURE);
 	node = get_env_node(minish->env, key);
 	if (!node)
 		return (EXIT_FAILURE);
@@ -31,6 +33,8 @@ static int	_append_value(char *key, char *value, t_minish *minish)
 	t_env	*node;
 	char	*join_str;
 
+	if (!is_valid_key(key))
+		return (EXIT_FAILURE);
 	node = get_env_node(minish->env, key);
 	join_str = NULL;
 	if (!node)
@@ -48,6 +52,8 @@ static int	_add_key_value(char *key, char *value, t_minish *minish)
 {
 	t_env	*node;
 
+	if (!is_valid_key(key))
+		return (EXIT_FAILURE);
 	node = ft_calloc(1, sizeof(t_env));
 	if (!node)
 		return (EXIT_FAILURE);
@@ -66,6 +72,8 @@ static int	_add_key(char *key, t_minish *minish)
 {
 	t_env	*node;
 
+	if (!is_valid_key(key))
+		return (EXIT_FAILURE);
 	node = ft_calloc(1, sizeof(t_env));
 	if (!node)
 		return (EXIT_FAILURE);
