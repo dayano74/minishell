a7f6128af203994de4d3ebc777d8c9b927553bf1
dayano74 <kiixxn00@gmail.com>
2025-05-01 15:51:21 +0900

    fix builtin format

--- inc/builtin.h
+++ inc/builtin.h
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 17:19:01 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/01 14:20:11 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/01 15:48:19 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@
 # include "builtin_utils.h"
 # include "struct.h"
 
-int	builtin_echo(int argc, char *argv[]);
-int	builtin_pwd(int argc, char *argv[]);
-int	builtin_exit(int argc, char *argv[]);
-int	builtin_cd(int argc, char *argv[]);
-int	builtin_env(int argc, char **argv, t_minish *minish);
-int	builtin_unset(int argc, char *argv[], t_minish *minish);
-int	builtin_export(int argc, char **argv, t_minish *minish);
+typedef int	(*builtin)(int argc, char *argv[], t_minish *minish);
+
+int			builtin_echo(int argc, char *argv[], t_minish *minish);
+int			builtin_pwd(int argc, char *argv[], t_minish *minish);
+int			builtin_exit(int argc, char *argv[], t_minish *minish);
+int			builtin_cd(int argc, char *argv[], t_minish *minish);
+int			builtin_env(int argc, char **argv, t_minish *minish);
+int			builtin_unset(int argc, char *argv[], t_minish *minish);
+int			builtin_export(int argc, char **argv, t_minish *minish);
 
 #endif
