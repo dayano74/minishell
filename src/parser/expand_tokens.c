cda51c435e52528597edc6ca659a110689e7180e
cacapon <cacapon.project@gmail.com>
2025-05-02 17:53:27 +0900

    $も展開するように修正

--- src/parser/expand_tokens.c
+++ src/parser/expand_tokens.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 17:08:42 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/02 17:22:05 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/05/02 17:52:43 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,49 @@ static size_t	_tokens_len(char **tokens)
 	return (i);
 }
 
+/**
+ * @note pre: $? in str
+ */
+static char	*_expand_dollar(char *str, t_minish *minish)
+{
+	char	*pre;
+	char	*suf;
+	char	*sts_str;
+	char	*joined;
+	char	*final;
+
+	pre = ft_substr(str, 0, ft_strlen_until(str, '$'));
+	sts_str = ft_itoa(minish->last_status);
+	suf = ft_strdup(str + ft_strlen_until(str, '$') + 2);
+	if (!pre || !sts_str || !suf)
+		return (free(pre), free(sts_str), free(suf), NULL);
+	joined = ft_strjoin(pre, sts_str);
+	final = ft_strjoin(joined, suf);
+	return (free(pre), free(sts_str), free(suf), free(joined), final);
+}
+
+static char	*_expand(char *token, t_minish *minish)
+{
+	char	*result;
+	char	*tmp;
+
+	result = ft_strdup(token);
+	if (!result)
+		return (NULL);
+	while (ft_strchr(result, '$'))
+	{
+		tmp = result;
+		if (ft_strnstr(result, "$?", ft_strlen(result)))
+			result = _expand_dollar(tmp, minish);
+		else
+			result = expand_env(tmp, minish);
+		free(tmp);
+		if (!result)
+			return (NULL);
+	}
+	return (result);
+}
+
 char	**expand_tokens(char **tokens, t_minish *minish)
 {
 	size_t	token_i;
@@ -35,7 +78,7 @@ char	**expand_tokens(char **tokens, t_minish *minish)
 	token_i = 0;
 	while (token_i < len)
 	{
-		expanded[token_i] = expand_env(tokens[token_i], minish);
+		expanded[token_i] = _expand(tokens[token_i], minish);
 		if (!expanded[token_i])
 		{
 			while (0 < token_i)
