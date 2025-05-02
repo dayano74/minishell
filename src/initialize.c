721488d3c8f14c06eed620af2efdb7502493a9b3
cacapon <cacapon.project@gmail.com>
2025-05-02 14:04:46 +0900

    _split_key_value -> _set_envp

--- src/initialize.c
+++ src/initialize.c
@@ -6,25 +6,28 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:42:46 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/01 13:46:27 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/05/02 14:04:39 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-static int	_split_key_value(char *env_val, char **key_out, char **val_out)
+static int	_set_envp(char *env_val, t_env *node)
 {
 	size_t	env_len;
 	size_t	eq_pos;
 
+	if (!env_val || !node)
+		return (1);
 	env_len = ft_strlen(env_val);
 	eq_pos = ft_strlen_until(env_val, '=');
-	if (eq_pos == ft_strlen(env_val))
+	if (eq_pos == env_len)
 		return (1);
-	*key_out = ft_substr(env_val, 0, eq_pos);
-	*val_out = ft_substr(env_val, eq_pos + 1, env_len - eq_pos - 1);
-	if (!*key_out || !*val_out)
+	node->key = ft_substr(env_val, 0, eq_pos);
+	node->value = ft_substr(env_val, eq_pos + 1, env_len - eq_pos - 1);
+	if (!node->key || !node->value)
 		return (1);
+	node->is_exported = 1;
 	return (0);
 }
 
@@ -68,7 +71,7 @@ static t_env	*create_envp_list(char **envp)
 		node = ft_calloc(1, sizeof(t_env));
 		if (!node)
 			return (NULL);
-		if (_split_key_value(envp[i], &(node->key), &(node->value)))
+		if (!_set_envp(envp[i], node))
 			return (_free_nodes(&head), _free_node(node), NULL);
 		node->next = NULL;
 		if (!head)
