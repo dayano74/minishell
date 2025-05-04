8efcd966363a6be30dac58173da3e73aca874b1b
dayano74 <kiixxn00@gmail.com>
2025-05-04 17:19:07 +0900

    update built-in exit

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 21:20:48 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/01 15:50:02 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/04 17:13:57 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 int	builtin_exit(int argc, char *argv[], t_minish *minish)
 {
 	(void)minish;
-	if (argc != 1)
+	if (argc > 2)
 		return (error_mes(argv[0], "too many arguments"), EXIT_FAILURE);
-	if (printf("exit\n") < 0)
-		return (perror("printf"), EXIT_FAILURE);
-	exit(EXIT_SUCCESS);
+	if(argc == 1)
+	{
+		if (printf("exit\n") < 0)
+			return (perror("printf"), EXIT_FAILURE);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		if (printf("exit\n") < 0)
+			return (perror("printf"), EXIT_FAILURE);
+		exit(argv[1]);
+	}
 }
