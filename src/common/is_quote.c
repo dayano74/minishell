db9c651b09706f68fa8cf19008c6e96f5932e299
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-05-05 14:54:22 +0900

    add is_quote

--- src/common/is_quote.c
+++ src/common/is_quote.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   common.h                                           :+:      :+:    :+:   */
+/*   is_quote.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/29 11:44:19 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/05 14:14:59 by ttsubo           ###   ########.fr       */
+/*   Created: 2025/05/05 14:07:03 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/05/05 14:15:15 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COMMON_H
-# define COMMON_H
+#include "common.h"
 
-# include "cmd.h"
-# include "error.h"
-# include "initialize.h"
-# include "libft.h"
-# include "struct.h"
-# include <stdbool.h>
-# include <stddef.h>
-
-bool	is_key_start(int c);
-bool	is_key_char(int c);
-bool	is_valid_key(char *str);
-bool	is_quote(char c);
-void	error_mes(char *name, char *mes);
-
-#endif
+bool	is_quote(char c)
+{
+	return (c == '\'' || c == '"');
+}
