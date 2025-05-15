b2ab0d3eff831632577e8b27c439e08a6dc323ce
dayano74 <kiixxn00@gmail.com>
2025-05-15 21:32:25 +0900

    fix norm

--- inc/invoke_cmd/redirect.h
+++ inc/invoke_cmd/redirect.h
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 15:20:03 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/15 21:17:43 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/15 21:30:20 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ bool	redirect_stdin(t_cmd *cmd);
 void	here_doc(char *limiter);
 
 // redirect_handler.c
-void handle_all_redirection(t_cmd *cmd, t_minish *minish);
+void	handle_all_redirection(t_cmd *cmd, t_minish *minish);
 
 #endif
--- src/invoke_cmd/redirect_handler.c
+++ src/invoke_cmd/redirect_handler.c
@@ -6,20 +6,20 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 21:12:09 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/15 21:23:22 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/15 21:31:16 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void handle_all_redirection(t_cmd *cmd, t_minish *minish)
+void	handle_all_redirection(t_cmd *cmd, t_minish *minish)
 {
-	t_cmd *redir;
+	t_cmd	*redir;
 
-	while(cmd->next && is_redirect(cmd->next))
+	while (cmd->next && is_redirect(cmd->next))
 	{
 		redir = cmd->next;
-		if(!redirect(redir))
+		if (!redirect(redir))
 			pipeline_exit(EXIT_FAILURE, minish);
 		cmd->next = cmd->next->next;
 		free_cmd(&redir);
