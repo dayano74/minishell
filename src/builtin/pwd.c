579b50d56835bb0b0423eddb8c709eea03f45e03
dayano74 <kiixxn00@gmail.com>
2025-05-15 20:42:19 +0900

    Fix pwd builtin

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 16:46:01 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/01 15:49:32 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/15 20:37:48 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ int	builtin_pwd(int argc, char *argv[], t_minish *minish)
 	char	pwd[PATH_MAX];
 
 	(void)minish;
-	if (argc != 1)
-	{
-		error_mes(argv[0], "wrong argument");
-		return (EXIT_FAILURE);
-	}
+	(void)argc;
 	if (getcwd(pwd, sizeof(pwd)) == NULL)
 	{
 		error_mes(argv[0], "cannot get working directory");
