9f43e25415f5b5e68332ef3bcce13f5f74ab19d5
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-05-17 12:30:57 +0900

    cdのHOME対応

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:12:37 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/01 15:49:07 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/17 12:30:45 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 int	builtin_cd(int argc, char *argv[], t_minish *minish)
 {
-	(void)minish;
-	if (argc != 2)
-		return (error_mes(argv[0], "wrong argument"), EXIT_FAILURE);
-	if (chdir(argv[1]) < 0)
-		return (perror(argv[1]), 1);
+	const char	*home = get_env_value(minish->env, "HOME");
+
+	if (!home)
+		return (error_mes("bash: cd", "HOME not set"), EXIT_FAILURE);
+	if (argc == 1)
+	{
+		if (chdir(home) < 0)
+			return (perror(home), 1);
+	}
+	else if (argc == 2)
+	{
+		if (chdir(argv[1]) < 0)
+			return (perror(argv[1]), 1);
+	}
+	else
+		return (error_mes(argv[0], "too many arguments"), EXIT_FAILURE);
 	return (0);
 }
