d80e5db2077bbdda758383b05430b5844ee4e9d8
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-05-17 13:31:09 +0900

    homeと-の処理を追加

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,29 +6,62 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:12:37 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/17 12:30:45 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/05/17 13:25:56 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int	builtin_cd(int argc, char *argv[], t_minish *minish)
+static int _cd_common(t_minish *minish, char *path)
 {
-	const char	*home = get_env_value(minish->env, "HOME");
+	char	*pwd;
+	char	*newpwd;
 
+	pwd = get_env_value(minish->env, "PWD");
+	if (chdir(path) < 0)
+		return (perror(path), 1);
+	newpwd = getcwd(NULL, 0);
+	if (!newpwd)
+		return (perror("getcwd"), 1);
+	set_env_value(minish->env, "OLDPWD", pwd);
+	set_env_value(minish->env, "PWD", newpwd);
+	free_str(&newpwd);
+	return (0);
+}
+
+static int _cd_home(t_minish *minish)
+{
+	char	*home;
+	
+	home = get_env_value(minish->env, "HOME");
 	if (!home)
 		return (error_mes("bash: cd", "HOME not set"), EXIT_FAILURE);
+	return (_cd_common(minish, home));
+}
+
+static int _cd_oldpwd(t_minish *minish)
+{
+	char	*oldpwd;
+
+	oldpwd = get_env_value(minish->env, "OLDPWD");
+	if (!oldpwd)
+		return (error_mes("bash: cd", "OLDPWD not set"), EXIT_FAILURE);
+	return (_cd_common(minish, oldpwd));
+}
+
+static int _cd_normal(char *path, t_minish *minish)
+{
+	return (_cd_common(minish, path));
+}
+
+int	builtin_cd(int argc, char *argv[], t_minish *minish)
+{
 	if (argc == 1)
-	{
-		if (chdir(home) < 0)
-			return (perror(home), 1);
-	}
+		return (_cd_home(minish));
+	else if (argc == 2 && ft_strcmp("-", argv[1]) == 0)
+		return (_cd_oldpwd(minish));
 	else if (argc == 2)
-	{
-		if (chdir(argv[1]) < 0)
-			return (perror(argv[1]), 1);
-	}
+		return (_cd_normal(argv[1], minish));
 	else
 		return (error_mes(argv[0], "too many arguments"), EXIT_FAILURE);
-	return (0);
 }
