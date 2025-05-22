722d403f3ec2922ce0fea3c88533238f977b96a8
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-05-22 12:51:26 +0900

    cd_oldpwdのとき、OLDPWDをdupするように修正

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:12:37 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/22 12:38:42 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/05/22 12:50:37 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,14 +55,14 @@ static int	_cd_oldpwd(t_minish *minish)
 	char	*oldpwd;
 	int		status;
 
-	oldpwd = get_env_value(minish->env, "OLDPWD");
+	oldpwd = ft_strdup(get_env_value(minish->env, "OLDPWD"));
 	if (!oldpwd)
 		return (error_mes("bash: cd", "OLDPWD not set"), EXIT_FAILURE);
 	status = _cd_common(minish, oldpwd);
 	if (status == 0)
-		return (printf("%s\n", oldpwd), status);
-	else
-		return (status);
+		printf("%s\n", oldpwd);
+	free_str(&oldpwd);
+	return (status);
 }
 
 static int	_cd_normal(char *path, t_minish *minish)
